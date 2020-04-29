7f44e9edf4e3ffec7c2855ac00dfbdd9bb19b451
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-29 20:24:55 +0900

    レイアウト調整

--- app/src/main/java/com/example/doubleroulette/RouletteDataEditActivity.kt
+++ app/src/main/java/com/example/doubleroulette/RouletteDataEditActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class RouletteDataEditActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_roulette_data_edit)
