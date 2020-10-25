439186beec0bb175374ef7724cd9d9b972e71a2f
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-10-25 10:17:15 +0900

    久々コミット

--- app/src/main/java/com/example/doubleroulette/RouletteActivity.kt
+++ app/src/main/java/com/example/doubleroulette/RouletteActivity.kt
@@ -1,12 +1,51 @@
 package com.example.doubleroulette
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_roulette.*
 
 class RouletteActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_roulette)
+
+        //描画できる
+        setContentView(DrawView(this))
+    }
+
+    // Viewを継承したクラス
+    internal inner class DrawView(context: Context) : View(context) {
+        private var paint: Paint = Paint()
+
+        // 描画するラインの太さ
+        private val lineStrokeWidth = 20f
+
+        init {
+        }
+
+        override fun onDraw(canvas: Canvas){
+
+            // ペイントする色の設定
+            paint.color = Color.argb(255, 255, 0, 255)
+
+            // ペイントストロークの太さを設定
+            paint.strokeWidth = lineStrokeWidth
+
+            // Styleのストロークを設定する
+            paint.style = Paint.Style.STROKE
+
+            // drawRectを使って矩形を描画する、引数に座標を設定
+            // (x1,y1,x2,y2,paint) 左上の座標(x1,y1), 右下の座標(x2,y2)
+            canvas.drawRect(300f, 300f, 600f, 600f, paint)
+        }
     }
 }
